03a17512077a7a7c6c3c451bbd25134372568dd9
Mark Wen <markwenguojie94@gmail.com>
2018-12-09 17:08:20 -0500

    Get Formik working with antd input

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,21 +1,20 @@
-import * as React from 'react'
-import { Component } from 'react'
-import { SafeAreaView } from 'react-native'
-import { Formik, Field } from 'formik'
-import { Constants } from 'expo'
-
-import FormikInput from './FormikInput'
+import * as React from "react";
+import { Component } from "react";
+import { SafeAreaView } from "react-native";
+import { Formik, FormikProps } from "formik";
+import { Constants } from "expo";
+import { InputItem, Button, List } from "antd-mobile-rn";
 
 interface SearchFormValues {
-  search: string
+  search: string;
 }
 
-const SearchInitialValue = { search: '' }
+const SearchInitialValue = { search: "" };
 
 export default class Search extends Component {
   handleSearchSubmit = (values: SearchFormValues) => {
-    console.log(values)
-  }
+    console.log(values);
+  };
 
   render() {
     return (
@@ -24,11 +23,32 @@ export default class Search extends Component {
           initialValues={SearchInitialValue}
           onSubmit={this.handleSearchSubmit}
         >
-          {() => (
-              <Field name="search" component={FormikInput} />
+          {({
+            values,
+            handleChange,
+            handleBlur,
+            handleSubmit
+          }: FormikProps<SearchFormValues>) => (
+            <List>
+              <InputItem
+                value={values.search}
+                onChange={handleChange("search")}
+                onBlur={handleBlur}
+                placeholder="Search"
+                defaultValue={SearchInitialValue.search}
+                extra={
+                <Button
+                  onClick={handleSubmit}
+                  type="primary"
+                >
+                  Search
+                </Button>
+              }
+              />
+            </List>
           )}
         </Formik>
       </SafeAreaView>
-    )
+    );
   }
 }
