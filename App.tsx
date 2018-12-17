ffd157811ebda2f4809d15112c24227dc4a686e6
Mark Wen <markwenguojie94@gmail.com>
2018-12-16 19:43:13 -0500

    Added Android font work-around

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
 import { Component } from 'react';
 import { Text, SafeAreaView } from 'react-native';
+import { Font, AppLoading } from 'expo';
 
 import Search from './src/components/Search';
 
 export default class App extends Component {
+  state = {
+    loading: true,
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+    });
+    this.setState({ loading: false });
+  }
+
   render() {
+    if (this.state.loading) {
+      return <AppLoading />;
+    }
+  
     return (
       <SafeAreaView>
         <Search />
